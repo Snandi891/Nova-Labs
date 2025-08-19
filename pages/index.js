ea02ea3a16abe3d07a759ebf0cdbde15f46a3a4e
Snandi891 <nitunandi1985@gmail.com>
2025-08-19 02:36:28 -0700

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -352,7 +352,7 @@ const App = () => {
   ];
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
+    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 flex items-center justify-center px-4 sm:px-6 lg:px-8">
       <Head>
         <title>DevSolutions - Premium Websites & Apps</title>
         <meta
