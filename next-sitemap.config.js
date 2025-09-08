445c261a5fa3af19e9b3c633456faf0fb2fd192e
Snandi891 <nitunandi1985@gmail.com>
2025-09-07 22:04:42 -0700

    Initial commit

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://nova-labs-puce.vercel.app", // 👈 your live site URL
+  generateRobotsTxt: true, // also creates robots.txt file
+};
