10429e875b165cfae58e526fc30351ebcabd2af8
Snandi891 <nitunandi1985@gmail.com>
2025-09-07 21:22:36 -0700

    Initial commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* ✅ Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="Rp8ch_A0AdO8zWFIkG7L_o2-h4hyRNeKVn5Vo8EZDwo"
+        />
+
         {/* ✅ Mobile viewport fix */}
         <meta
           name="viewport"
