a8a3377db15b929789afd6f5f027d5bf2b609b5d
Snandi891 <nitunandi1985@gmail.com>
2025-09-07 21:27:53 -0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        {/* ✅ Google Site Verification */}
+        <meta
+          name="google-site-verification"
+          content="Rp8ch_A0AdO8zWFIkG7L_o2-h4hyRNeKVn5Vo8EZDwo"
+        />
+      </Head>
       <Component {...pageProps} />
       <Toaster position="top-right" reverseOrder={false} />
     </>
